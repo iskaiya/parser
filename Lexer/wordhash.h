19a357b846ad4ac1461ceb4150a8359e7f8ae42d
Skai <iskabols@gmail.com>
2025-11-10 22:18:15 +0800

    Added parser files

---
+++ Lexer/wordhash.h
@@ -0,0 +1,25 @@
+#ifndef WORDHASH_H
+#define WORDHASH_H
+
+#include <stdbool.h>
+#include "tokens.h"
+
+//HASH TABLE STUFF
+#define TABLE_SIZE 100
+#define MAX 100
+
+typedef struct {
+    char key[MAX];
+    TokenCategory category;
+    int tokenValue;
+} HashEntry;
+
+extern HashEntry hash_table[TABLE_SIZE]; //declare hash table
+
+//function prototypes
+unsigned int hash(const char *key);
+void hashhashInsert(const char *key, TokenCategory category, int token_value);
+HashEntry* hashLookUp(const char *key);
+void initialize_table(void);
+
+#endif
